92640e86170f9db67e45284c8778d5f7964ab817
Mehmet Eker <48644796+infotus@users.noreply.github.com>
2024-06-21 21:36:42 +0900

    no blur malino text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,12 +8,12 @@ const Main = () => {
     return(
         <div id="main">
             <img className='w-full h-screen object-cover object-center' src={backgroundImg}/>
-            <div >
+            <div className='w-full h-screen absolute top-0 left-0 '>
                 <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center ld:items-start items-center'>
-                <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>
+                <h1 className='sm:text-5xl text-4xl font-bold text-green-800'>
                     MALINO
                 </h1>
-                <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
+                <h2 className='flex sm:text-3xl text-2xl pt-4 text-green-800'>
                     
                     <TypeAnimation
                         sequence={[
@@ -32,13 +32,13 @@ const Main = () => {
                         repeat={Infinity}
                     />
                 </h2>
-                <div className='flex justify-between pt-6 max-w-[200px] w-full'>
+                {/* <div className='flex justify-between pt-6 max-w-[200px] w-full'>
                     <FaInstagram className="cursor-pointer" size={20}/>
                     <FaFacebook className="cursor-pointer" size={20}/>
                     <FaXTwitter className="cursor-pointer" size={20}/>
                     <FaSquareThreads className="cursor-pointer" size={20}/>
                     <FaYoutube className="cursor-pointer" size={20}/>
-                </div>
+                </div> */}
                 </div>
             </div>
         </div>
