5f4cc9bff8dc7bdac9313e1d5445a39271bb8d07
Mehmet Eker <48644796+infotus@users.noreply.github.com>
2024-06-26 21:44:23 +0900

    slogan updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import backgroundImg from "../assets/future-with-ai.png"
+import logo from '../assets/logotemp.png'
 import {TypeAnimation} from "react-type-animation";
 import {FaFacebook, FaInstagram, FaXTwitter, FaYoutube, FaSquareThreads} from "react-icons/fa6";
 
@@ -10,20 +11,20 @@ const Main = () => {
             <img className='w-full h-screen object-cover object-center' src={backgroundImg}/>
             <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
                 <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center ld:items-start items-center'>
-                <h1 className='sm:text-5xl text-4xl font-bold text-cyan-800'>
+                <h1 className='sm:text-5xl text-4xl font-bold text-cyan-800 '>
                     HIVIAI
                 </h1>
                 <h2 className='p-10 flex sm:text-3xl text-2xl pt-4 text-cyan-800'>
                     
                     <TypeAnimation
                         sequence={[
-                            'Empowering',
+                            'The first step',
                             2000, 
-                            'Innovation',
+                            'to reaching',
                             2000,
-                            'Transforming',
+                            'your dreams',
                             2000,
-                            'Tommorrow',
+                            'Hiviai',
                             2000,
                         ]}
                         wrapper="div"
@@ -32,6 +33,7 @@ const Main = () => {
                         repeat={Infinity}
                     />
                 </h2>
+
                 {/* <div className='flex justify-between pt-6 max-w-[200px] w-full'>
                     <FaInstagram className="cursor-pointer" size={20}/>
                     <FaFacebook className="cursor-pointer" size={20}/>
